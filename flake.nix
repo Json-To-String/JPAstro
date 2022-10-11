fffac8133b130059e06257cc3cb7a6ead04eb13e
Pranav <pranav@myrdd.info>
2022-10-10 21:18:57 -0700

    Oct 10th post meeting

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    mach-nix.url = "mach-nix/3.5.0";
+  };
+
+  outputs = {self, nixpkgs, mach-nix }@inp:
+    let
+      l = nixpkgs.lib // builtins;
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      forAllSystems = f: l.genAttrs supportedSystems
+        (system: f system (import nixpkgs {inherit system;}));
+    in
+    {
+      # enter this python environment by executing `nix shell .`
+      defaultPackage = forAllSystems (system: pkgs: mach-nix.lib."${system}".mkPython {
+        requirements = ''
+          networkx
+          numpy
+	  sympy
+          matplotlib
+          jupyterlab
+	  pandas
+ 	  requests
+  	  torch
+	  scikit-image
+        '';
+      });
+    };
+}
+
